c65423ff1e549a8dcedf1f931bbf5898bddd0c4a
Duskafka <rlarlgus0206@naver.com>
2025-07-22 11:47:51 +0900

    docs: CodeQL에서 정상적으로 오류를 잡아주는 것을 확인하고 오류를 잡았던 내용을 문서화
    - 깃허브 액션에서 오류가 난 지점의 로그를 기록함.

--- src/main/java/com/githubactions/MyTestService.java
+++ src/main/java/com/githubactions/MyTestService.java
@@ -11,7 +11,45 @@ public class MyTestService {
         log.info("[MyTestService] sayHello");
     }
 
+    /**
+     * Starting Gradle build for CodeQL analysis...
+     * Welcome to Gradle 8.14.3!
+     * Here are the highlights of this release:
+     *  - Java 24 support
+     *  - GraalVM Native Image toolchain selection
+     *  - Enhancements to test reporting
+     *  - Build Authoring improvements
+     * For more details see https://docs.gradle.org/8.14.3/release-notes.html
+     * Starting a Gradle Daemon (subsequent builds will be faster)
+     * > Task :compileJava FAILED
+     * /home/runner/work/Github-Actions-Practice/Github-Actions-Practice/src/main/java/com/githubactions/MyTestService.java:15: error: not a statement
+     *         error...
+     *         ^
+     * /home/runner/work/Github-Actions-Practice/Github-Actions-Practice/src/main/java/com/githubactions/MyTestService.java:15: error: ';' expected
+     *         error...
+     * [Incubating] Problems report is available at: file:///home/runner/work/Github-Actions-Practice/Github-Actions-Practice/build/reports/problems/problems-report.html
+     * 1 actionable task: 1 executed
+     *              ^
+     * 2 errors
+     * FAILURE: Build failed with an exception.
+     * * What went wrong:
+     * Execution failed for task ':compileJava'.
+     * > Compilation failed; see the compiler output below.
+     *   /home/runner/work/Github-Actions-Practice/Github-Actions-Practice/src/main/java/com/githubactions/MyTestService.java:15: error: not a statement
+     *           error...
+     *           ^
+     *   /home/runner/work/Github-Actions-Practice/Github-Actions-Practice/src/main/java/com/githubactions/MyTestService.java:15: error: ';' expected
+     *           error...
+     *                ^
+     *   2 errors
+     * * Try:
+     * > Check your code and dependencies to fix the compilation error(s)
+     * > Run with --scan to get full insights.
+     * BUILD FAILED in 13s
+     * Error: Process completed with exit code 1.
+     * */
     public void error(){
-        error...
+        // CodeQL에서 정상적으로 오류를 잡음
+        //error...
     }
 }
