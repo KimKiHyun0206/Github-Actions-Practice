137c5890281a2401b3843a97dd5e11bf7eef8c94
Duskafka <rlarlgus0206@naver.com>
2025-07-22 11:41:47 +0900

    feat: CodeQL이 검사할 수 있는 클래스 추가

---
+++ src/main/java/com/githubactions/MyTestService.java
@@ -0,0 +1,13 @@
+package com.githubactions;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class MyTestService {
+
+    public void sayHello() {
+        log.info("[MyTestService] sayHello");
+    }
+}
